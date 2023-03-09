177ed77d5b3ce9d0a216f71fa96dafabfe44b13b
cemtaskin <cemtaskin82@gmail.com>
2023-03-09 20:24:42 +0300

    add new method with a parameter

--- Methods.java
+++ Methods.java
@@ -3,11 +3,16 @@ public class Methods {
     static void myMethod (){
         System.out.println("I just executed");
     }
+    
+    static void sayHello (String name){
+        System.out.println(name);
+    }
+
 
     public static void main(String[] args) {
         myMethod();
+        sayHello("Ali Duru");
         myMethod();
-        myMethod();
-           
+        sayHello("Aliye Duru");      
     }
 }
