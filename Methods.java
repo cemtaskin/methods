211e52e4b2a9239f0eb261a86d110fd47faf4277
cemtaskin <cemtaskin82@gmail.com>
2023-03-09 20:27:53 +0300

    functions with multiple parameters

--- Methods.java
+++ Methods.java
@@ -7,12 +7,20 @@ public class Methods {
     static void sayHello (String name){
         System.out.println(name);
     }
+    
+    static void sayHello (String name,int count){
+        for (int i=0;i<count;i++){
+            System.out.println(name);
+        }
+    }
+
 
 
     public static void main(String[] args) {
         myMethod();
         sayHello("Ali Duru");
         myMethod();
-        sayHello("Aliye Duru");      
+        sayHello("Aliye Duru");  
+        sayHello("Ali Duru",105);    
     }
 }
