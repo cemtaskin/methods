b67179caa6dc9651a0476be723a746043fe8cd15
cemtaskin <cemtaskin82@gmail.com>
2023-03-09 20:16:36 +0300

    intial commit

---
+++ Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+
+    static void myMethod (){
+        System.out.println("I just executed");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+           
+    }
+}
